5866dfddeb23babb8e3a976c5cda6fe518460a45
clayton sims <csims@dimagi.com>
2012-07-17 13:55:02 -0400

    Added hooks for catching issues with security permissions on j2me

--- j2me/communication/src/org/javarosa/j2me/reference/HttpReference.java
+++ j2me/communication/src/org/javarosa/j2me/reference/HttpReference.java
@@ -19,9 +19,15 @@ import org.javarosa.core.reference.Reference;
 public class HttpReference implements Reference {
 
 	String URI;
+	SecurityFailureListener listener;
 	
 	public HttpReference(String URI) {
+		this(URI, null);
+	}
+	
+	public HttpReference(String URI, SecurityFailureListener listener) {
 		this.URI = URI;
+		this.listener = listener;
 	}
 
 	/* (non-Javadoc)
@@ -33,16 +39,23 @@ public class HttpReference implements Reference {
 	}
 	
 	public InputStream getStream() throws IOException {
-		HttpConnection connection = (HttpConnection)Connector.open(URI);
-		connection.setRequestMethod(HttpConnection.GET);
-		InputStream httpStream = connection.openInputStream();
-		
-		//This actually signals the connection to close as soon as the input stream
-		//does, which we need, since after we pass it out, we have no way to manage
-		//the connection.
-		connection.close();
-		
-		return httpStream;
+		try {
+			HttpConnection connection = (HttpConnection)Connector.open(URI);
+			connection.setRequestMethod(HttpConnection.GET);
+			InputStream httpStream = connection.openInputStream();
+			
+			//This actually signals the connection to close as soon as the input stream
+			//does, which we need, since after we pass it out, we have no way to manage
+			//the connection.
+			connection.close();
+			
+			return httpStream;
+		} catch(SecurityException se) {
+			if(this.listener != null) {
+				listener.onSecurityException(se);
+			}
+			throw new IOException("Couldn't retrieve data from " + this.getLocalURI() + " due to lack of permissions.");
+		}
 	}
 	
 	public String getURI() {
@@ -71,5 +84,10 @@ public class HttpReference implements Reference {
 	
 	public Reference[] probeAlternativeReferences() {
 		return new Reference [0];
+	}
+
+
+	public interface SecurityFailureListener {
+		public void onSecurityException(SecurityException e);
 	}
 }
--- j2me/communication/src/org/javarosa/j2me/reference/HttpRoot.java
+++ j2me/communication/src/org/javarosa/j2me/reference/HttpRoot.java
@@ -5,19 +5,23 @@ package org.javarosa.j2me.reference;
 
 import org.javarosa.core.reference.PrefixedRootFactory;
 import org.javarosa.core.reference.Reference;
+import org.javarosa.j2me.reference.HttpReference.SecurityFailureListener;
 
 /**
  * @author ctsims
  *
  */
 public class HttpRoot extends PrefixedRootFactory {
+	
+	SecurityFailureListener listener;
 
-	public HttpRoot() {
+	public HttpRoot(SecurityFailureListener listener) {
 		super(new String[] {"http://","https://"});
+		this.listener = listener;
 	}
 	
 
 	protected Reference factory(String terminal, String URI) {
-		return new HttpReference(URI);
+		return new HttpReference(URI, listener);
 	}
 }
