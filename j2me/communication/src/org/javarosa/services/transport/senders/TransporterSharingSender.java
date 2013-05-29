5c982b23036dbedd80546753e3181bc5775b80be
clayton sims <csims@dimagi.com>
2013-05-29 13:27:03 -0400

    Introduced a maximum number of failures which can occur before the send all unsent process stops trying to send the rest.
    --HG--
    extra : source : 5f6c0f6d4f8e68cbe9bfc52937c6c46ad6f731f5

--- j2me/communication/src/org/javarosa/services/transport/senders/TransporterSharingSender.java
+++ j2me/communication/src/org/javarosa/services/transport/senders/TransporterSharingSender.java
@@ -14,24 +14,34 @@ public class TransporterSharingSender {
 	private TransportCache cache;
 	private TransportListener listener;
 	boolean halted = false;
+	private int maxConsecutiveFails = -1;
 
 	public TransporterSharingSender() {
 	}
 	
-	public void init(Vector messages, TransportCache store, TransportListener listener) {
+	public void init(Vector messages, TransportCache store, TransportListener listener, int maxConsecutiveFails) {
 		this.messages = messages;
 		this.cache = store;
 		this.listener = listener;
 		halted = false;
+		this.maxConsecutiveFails =maxConsecutiveFails;
 	}
 
 	public void send() {
 		System.out.println("Ready to send: "+this.messages.size()+" messages");
 		
 		int numSuccessful = 0;
+		int consecutiveFailures = 0;
 		for (int i = 0; i < this.messages.size(); i++) {
 			if(halted) {return;}
+			
 			TransportMessage message = (TransportMessage) this.messages.elementAt(i);
+			
+			//If we're over our limit, jut go ahead and update the listener for the rest of the messages.
+			if(maxConsecutiveFails != -1 && consecutiveFailures >= maxConsecutiveFails) {
+				this.listener.onStatusChange(message);
+				continue;
+			}
 
 			this.listener.onChange(message, "Preparing to send: " + message);
 			message.send();
@@ -41,6 +51,7 @@ public class TransporterSharingSender {
 				// used up, then the message becomes cached, for sending
 				// via the "Send Unsent" user function
 				if (!message.isSuccess()) {
+					consecutiveFailures++;
 					Logger.log("send-all", "fail on " + (i + 1) + "/" + messages.size() + " " + message.getFailureReason());
 					
 					message.setStatus(TransportMessageStatus.CACHED);
@@ -62,6 +73,7 @@ public class TransporterSharingSender {
 					}
 				} else {
 					numSuccessful++;
+					consecutiveFailures = 0;
 					
 					// SUCCESS - remove from cache
 					this.listener.onStatusChange(message);
@@ -77,7 +89,15 @@ public class TransporterSharingSender {
 				Logger.log("sanity", "TransportSharingSender.send msg not cacheable");
 			}
 		}
-		Logger.log("send-all", (numSuccessful == messages.size() ? "success" : numSuccessful + "/" + messages.size() + " successful"));
+		
+		String logMsg = (numSuccessful == messages.size() ? "success" : numSuccessful + "/" + messages.size() + " successful");
+		String logTag = "send-all-success";
+		if(maxConsecutiveFails != -1 && consecutiveFailures >= maxConsecutiveFails) {
+			//We stopped because we failed too many times in a row.
+			logMsg = "Send cancelled after " + consecutiveFailures + " failures. " + logMsg;
+			logTag = "send-all-fail";
+		}
+		Logger.log(logTag, logMsg);
 	}
 	
 	public void halt() {
