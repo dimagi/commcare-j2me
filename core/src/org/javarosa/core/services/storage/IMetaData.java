a739036e1799cd972542910070b12f6b107c4674
ALIZA STONE <amstone326@gmail.com>
2014-07-29 09:03:52 -0400

    added all files from local repo

---
+++ core/src/org/javarosa/core/services/storage/IMetaData.java
@@ -0,0 +1,12 @@
+package org.javarosa.core.services.storage;
+
+import java.util.Hashtable;
+
+public interface IMetaData {
+
+	//for the indefinite future, no meta-data field can have a value of null
+	
+	String[] getMetaDataFields ();
+	Object getMetaData(String fieldName);
+	
+}
